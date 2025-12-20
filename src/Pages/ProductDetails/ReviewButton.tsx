7602dd4fba207b30f9e80178fee8f753e8a6b9e9
salma <slmasamytaha@gmail.com>
2025-12-20 11:15:49 +0200

    add to cart

--- src/Pages/ProductDetails/ReviewButton.tsx
+++ src/Pages/ProductDetails/ReviewButton.tsx
@@ -9,7 +9,7 @@ export default function ReviewButton({isOpen}:Props) {
    const isRtl = i18n.language === "ar"
   return (
     <div className='flex justify-end mb-2'>
-            <button className={`flex items-center gap-2  rounded-2xl shadow-md bg-[var(--color-light-accent)] text-[var(--color-light-dark)] px-4 py-2   ${isRtl ? 'ml-3' : 'mr-3'}`} onClick={isOpen}>
+            <button className={`flex items-center gap-2  rounded-2xl shadow-md bg-[var(--color-light-accent)] text-[var(--color-light-dark)] px-4 py-2 hover:bg-[var(--color-accent-dark)] hover:text-white  ${isRtl ? 'ml-3' : 'mr-3'}`} onClick={isOpen}>
                <MdAdd className="text-2xl " />
               <span className=' text-lg font-bold'>{t("ProductDetails.addReview")}</span>
             </button>
--- src/Pages/ProductDetails/ToggleButton.tsx
+++ src/Pages/ProductDetails/ToggleButton.tsx
@@ -11,7 +11,7 @@ export default function ToggleButton({isOpen,onClick}:Props) {
   return (
      <div className='flex justify-center '>
           <button
-              className="flex items-center gap-2 bg-gradient-to-r from-[var(--color-light-accent)] to-[var(--color-accent-dark)] text-[var(--color-light-dark)] font-bold px-4 py-3 rounded-3xl shadow-xl hover:shadow-2xl hover:from-[var(--color-accent-dark)] hover:to-[var(--color-light-accent)] transition-all duration-300 transform hover:scale-105"
+              className="flex items-center gap-2 bg-gradient-to-r from-[var(--color-light-accent)] to-[var(--color-accent-dark)] text-[var(--color-light-dark)] font-bold px-4 py-3 rounded-3xl shadow-xl hover:shadow-2xl hover:from-[var(--color-accent-dark)] hover:to-[var(--color-light-accent)] hover:text-white transition-all duration-300 transform hover:scale-105"
           onClick={onClick}
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
